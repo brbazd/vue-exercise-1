46b86257097e0601ade4cf8df8b5db47bde2a6a8
brbazd <branko.bazdar@gmail.com>
2023-10-02 17:25:01 +0200

    first

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<script>
+
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.count++
+            },
+            decrement() {
+                this.count--
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h1>{{ count }}</h1>
+        <button v-on:click="increment">+</button>
+        <button v-on:click="decrement">-</button>
+    </div>
+</template>
